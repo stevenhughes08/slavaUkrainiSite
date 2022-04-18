0c35f3b66cd86bbb85b50f79824a9fe958419a53
stevenhughes08 <stevenhughes08@gmail.com>
2022-04-17 23:29:24 -0400

    added responsive navbar

---
+++ public/js/mobileMenu.js
@@ -0,0 +1,4 @@
+const McButton = document.querySelector('[data=hamburger-menu]');
+const McBar1 = McButton.find('b:nth-child(1)');
+const McBar2 = McButton.find('b:nth-child(2)');
+const McBar3 = McButton.find('b:nth-child(3)');
